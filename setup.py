a47e41044ca291ade98208d865e23f8e110ba940
palewire <b@palewi.re>
2022-07-06 09:41:56 -0700

    Fix makefile

--- setup.py
+++ setup.py
@@ -72,15 +72,45 @@ def read(fname):
         return f.read()
 
 
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
 setup(
     name="django-internetarchive-storage",
-    version="0.0.1",
     description="A custom Django storage system for Internet Archive collections",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Ben Welsh",
     author_email="b@palewi.re",
-    url="http://www.github.com/california-civic-data-coalition/django-internetarchive-storage",
+    url="https://palewi.re/docs/django-internetarchive-storage",
     license="MIT",
     packages=("ia_storage",),
     install_requires=[
@@ -92,12 +122,16 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
     project_urls={
-        "Maintainer": "https://github.com/california-civic-data-coalition",
-        "Source": "https://github.com/datadesk/california-civic-data-coalition/django-internetarchive-storage",
-        "Tracker": "https://github.com/datadesk/<california-civic-data-coalition/django-internetarchive-storage/issues",
+        "Maintainer": "https://github.com/palewire",
+        "Source": "https://github.com/palewire/django-internetarchive-storage",
+        "Tracker": "https://github.com/palewire/django-internetarchive-storage/issues",
+        "Docs": "https://palewi.re/docs/django-internetarchive-storage",
     },
     cmdclass={"test": TestCommand},
+    setup_requires=["setuptools_scm"],
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
 )
